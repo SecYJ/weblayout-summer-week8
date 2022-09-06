562cfcf07cd1bf9d81967cf6366d33dbec9e0b05
Sec <qianqian1903@gmail.com>
2022-09-07 00:15:55 +0800

    added tab switch for showcase page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,23 +1,35 @@
 window.addEventListener("DOMContentLoaded", () => {
-    // init Masonry
-    // || "collection.grid"
-    const artworkGrid = document.querySelector(".artwork-grid");
-    const artworkMasonry = new Masonry(artworkGrid, {
-        itemSelector: ".artwork-item",
-        columnWidth: ".artwork-sizer",
-        percentPosition: true,
-        gutter: ".artwork-gutter",
-        horizontalOrder: true,
-    });
-    imagesLoaded(artworkGrid).on("progress", function () {
-        // layout Masonry after each image loads
-        artworkMasonry.layout();
-    });
+    const masonryInit = (element, options) => {
+        const { gutter, columnWidth, itemSelector } = options;
+        const masonry = new Masonry(element, {
+            itemSelector,
+            columnWidth,
+            gutter,
+            percentPosition: true,
+            horizontalOrder: true,
+        });
+        imagesLoaded(element).on("progress", function () {
+            // layout Masonry after each image loads
+            masonry.layout();
+        });
+    };
+
+    const artworkGrid = document.querySelector("#artwork-grid");
+    console.log(artworkGrid);
+    if (artworkGrid) {
+        const options = {
+            itemSelector: ".artwork-item",
+            gutter: ".artwork-gutter",
+            columnWidth: ".artwork-sizer",
+        };
+        masonryInit(artworkGrid, options);
+    }
 
     const swiper = new Swiper(".swiper", {
         loop: true,
         slidesPerView: 1,
         freeMode: true,
+        resizeObserver: true,
         autoplay: {
             delay: 3000,
             disabledOnInteraction: false,
@@ -66,4 +78,56 @@ window.addEventListener("DOMContentLoaded", () => {
     mobileMenuBtn.addEventListener("click", () => {
         mobileNavMenu.classList.toggle("hidden");
     });
+
+    const artworkBtn = document.querySelector("#artwork-btn");
+    const collectionBtn = document.querySelector("#collection-btn");
+
+    const showcaseTabSwitch = (e) => {
+        const targetString =
+            e.target.dataset?.targetEl ||
+            e.target.parentElement.dataset.targetEl;
+        const closeEl =
+            targetString === "#collection-items"
+                ? "#artwork-items"
+                : "#collection-items";
+
+        document.querySelector(closeEl).classList.add("hidden");
+        document.querySelector(targetString).classList.remove("hidden");
+        document.querySelector(targetString).classList.add("border-black");
+
+        if (targetString === "#collection-items") {
+            const options = {
+                itemSelector: ".collection-item",
+                gutter: ".collection-gutter",
+                columnWidth: ".collection-sizer",
+            };
+
+            masonryInit("#collection-items", options);
+
+            artworkBtn.classList.remove("border-gray-dark");
+            artworkBtn.classList.remove("border-b-gray-light");
+            artworkBtn.classList.add("border-transparent");
+            artworkBtn.classList.add("border-b-gray-dark");
+            artworkBtn.classList.add("text-gray-dark");
+            collectionBtn.classList.remove("border-transparent");
+            collectionBtn.classList.add("border-gray-dark");
+            collectionBtn.classList.add("border-b-gray-light");
+            collectionBtn.classList.remove("text-gray-dark");
+            return;
+        }
+
+        collectionBtn.classList.remove("border-gray-dark");
+        collectionBtn.classList.remove("border-b-gray-light");
+        collectionBtn.classList.add("border-transparent");
+        collectionBtn.classList.add("border-b-gray-dark");
+        collectionBtn.classList.add("text-gray-dark");
+        artworkBtn.classList.add("border-gray-dark");
+        artworkBtn.classList.add("border-b-gray-light");
+        artworkBtn.classList.remove("border-transparent");
+        artworkBtn.classList.remove("border-b-gray-dark");
+        artworkBtn.classList.remove("text-gray-dark");
+    };
+
+    collectionBtn.addEventListener("click", showcaseTabSwitch);
+    artworkBtn.addEventListener("click", showcaseTabSwitch);
 });
