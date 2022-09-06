9f2b56e9a12c45c7b797e2dbbfacbaa5080479ba
Sec <qianqian1903@gmail.com>
2022-09-07 00:16:15 +0800

    Update 2022-09-06T16:16:12.278Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,24 +1,40 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function () {
-  // init Masonry
-  // || "collection.grid"
-  var artworkGrid = document.querySelector(".artwork-grid");
-  var artworkMasonry = new Masonry(artworkGrid, {
-    itemSelector: ".artwork-item",
-    columnWidth: ".artwork-sizer",
-    percentPosition: true,
-    gutter: ".artwork-gutter",
-    horizontalOrder: true
-  });
-  imagesLoaded(artworkGrid).on("progress", function () {
-    // layout Masonry after each image loads
-    artworkMasonry.layout();
-  });
+  var masonryInit = function masonryInit(element, options) {
+    var gutter = options.gutter,
+        columnWidth = options.columnWidth,
+        itemSelector = options.itemSelector;
+    var masonry = new Masonry(element, {
+      itemSelector: itemSelector,
+      columnWidth: columnWidth,
+      gutter: gutter,
+      percentPosition: true,
+      horizontalOrder: true
+    });
+    imagesLoaded(element).on("progress", function () {
+      // layout Masonry after each image loads
+      masonry.layout();
+    });
+  };
+
+  var artworkGrid = document.querySelector("#artwork-grid");
+  console.log(artworkGrid);
+
+  if (artworkGrid) {
+    var options = {
+      itemSelector: ".artwork-item",
+      gutter: ".artwork-gutter",
+      columnWidth: ".artwork-sizer"
+    };
+    masonryInit(artworkGrid, options);
+  }
+
   var swiper = new Swiper(".swiper", {
     loop: true,
     slidesPerView: 1,
     freeMode: true,
+    resizeObserver: true,
     autoplay: {
       delay: 3000,
       disabledOnInteraction: false
@@ -63,5 +79,50 @@ window.addEventListener("DOMContentLoaded", function () {
   mobileMenuBtn.addEventListener("click", function () {
     mobileNavMenu.classList.toggle("hidden");
   });
+  var artworkBtn = document.querySelector("#artwork-btn");
+  var collectionBtn = document.querySelector("#collection-btn");
+
+  var showcaseTabSwitch = function showcaseTabSwitch(e) {
+    var _e$target$dataset;
+
+    var targetString = ((_e$target$dataset = e.target.dataset) === null || _e$target$dataset === void 0 ? void 0 : _e$target$dataset.targetEl) || e.target.parentElement.dataset.targetEl;
+    var closeEl = targetString === "#collection-items" ? "#artwork-items" : "#collection-items";
+    document.querySelector(closeEl).classList.add("hidden");
+    document.querySelector(targetString).classList.remove("hidden");
+    document.querySelector(targetString).classList.add("border-black");
+
+    if (targetString === "#collection-items") {
+      var _options = {
+        itemSelector: ".collection-item",
+        gutter: ".collection-gutter",
+        columnWidth: ".collection-sizer"
+      };
+      masonryInit("#collection-items", _options);
+      artworkBtn.classList.remove("border-gray-dark");
+      artworkBtn.classList.remove("border-b-gray-light");
+      artworkBtn.classList.add("border-transparent");
+      artworkBtn.classList.add("border-b-gray-dark");
+      artworkBtn.classList.add("text-gray-dark");
+      collectionBtn.classList.remove("border-transparent");
+      collectionBtn.classList.add("border-gray-dark");
+      collectionBtn.classList.add("border-b-gray-light");
+      collectionBtn.classList.remove("text-gray-dark");
+      return;
+    }
+
+    collectionBtn.classList.remove("border-gray-dark");
+    collectionBtn.classList.remove("border-b-gray-light");
+    collectionBtn.classList.add("border-transparent");
+    collectionBtn.classList.add("border-b-gray-dark");
+    collectionBtn.classList.add("text-gray-dark");
+    artworkBtn.classList.add("border-gray-dark");
+    artworkBtn.classList.add("border-b-gray-light");
+    artworkBtn.classList.remove("border-transparent");
+    artworkBtn.classList.remove("border-b-gray-dark");
+    artworkBtn.classList.remove("text-gray-dark");
+  };
+
+  collectionBtn.addEventListener("click", showcaseTabSwitch);
+  artworkBtn.addEventListener("click", showcaseTabSwitch);
 });
 //# sourceMappingURL=all.js.map
